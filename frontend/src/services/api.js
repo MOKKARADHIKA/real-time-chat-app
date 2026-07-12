293d77c75e512399cacfc6f29f339f539eaaa489
MOKKA RADHIKA <mokkaradhika502@gmail.com>
2026-07-12 17:54:04 +0530

    prepare frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "https://real-time-chat-app-production-864f.up.railway.app"
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://real-time-chat-app-production-864f.up.railway.app"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default API;
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,10 +1,16 @@
+// import { io } from "socket.io-client";
+
+// const socket = io(
+//   "https://real-time-chat-app-production-864f.up.railway.app",
+//   {
+//     transports: ["websocket"]
+//   }
+// );
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-const socket = io(
-  "https://real-time-chat-app-production-864f.up.railway.app",
-  {
-    transports: ["websocket"]
-  }
-);
+const socket = io(import.meta.env.VITE_API_URL);
 
 export default socket;
